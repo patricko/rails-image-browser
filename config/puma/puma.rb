dc71a1d375e19f21698f86835d4adaa041010bb4
patricko <patrickod@gmail.com>
2018-04-11 20:22:35 -0700

    formatting

--- config/puma/puma.rb
+++ config/puma/puma.rb
@@ -1,16 +1,16 @@
-#!/usr/bin/env puma	
-	
-directory '/www/apps/rails-image-browser'	
-rackup "/www/apps/rails-image-browser/config.ru"	
-environment 'production'	
-	
-pidfile "/www/apps/rails-image-browser/tmp/pids/puma.pid"	
-state_path "/www/apps/rails-image-browser/tmp/pids/puma.state"	
-stdout_redirect '/var/log/puma/rails-image-browser/access.log', '/var/log/puma/rails-image-browser/error.log', true	
-	
-	
-# threads 0,4	
-	
-bind 'unix:///www/apps/rails-image-browser/tmp/sockets/puma.sock'	
-	
-# workers 2	
+#!/usr/bin/env puma
+# frozen_string_literal: true
+
+directory '/www/apps/rails-image-browser'
+rackup '/www/apps/rails-image-browser/config.ru'
+environment 'production'
+
+pidfile '/www/apps/rails-image-browser/tmp/pids/puma.pid'
+state_path '/www/apps/rails-image-browser/tmp/pids/puma.state'
+stdout_redirect '/var/log/puma/rails-image-browser/access.log', '/var/log/puma/rails-image-browser/error.log', true
+
+# threads 0,4
+
+bind 'unix:///www/apps/rails-image-browser/tmp/sockets/puma.sock'
+
+# workers 2
